f1750a129ae0f6640ee4cefe1c9b078e17de81c7
Eloïse <131976796+Elo-LH@users.noreply.github.com>
2024-05-28 14:54:47 +0200

    Feature/admin space editing users and search (#2)
    * feat: modify user from admin space
    
    * feat: create new user from admin space
    
    * feat: search for user in admin space

--- logic/register.php
+++ logic/register.php
@@ -13,6 +13,15 @@ if (!array_filter($_POST)) {
     $email = $_POST['email'];
     $password = $_POST['password'];
     $hash = password_hash($password, PASSWORD_DEFAULT);
+    //if a role has been given to register from admin user creation
+    if (isset($_POST['role'])) {
+
+        if ($_POST['role'] === 'null') {
+            $role = null;
+        } else {
+            $role = "admin";
+        };
+    };
 
     // Fetch if this email is allready in DB
     $query = $db->prepare('SELECT * FROM users WHERE email = :email');
@@ -27,16 +36,31 @@ if (!array_filter($_POST)) {
         header('Location: ../index.php?route=inscription&error=' . $email);
     } else {
         // If not found, create an new user in DB
-        $query = $db->prepare('INSERT INTO users(pseudo, email, password, avatar) VALUES(:pseudo, :email, :password, :avatar)');
-        $parameters = [
-            'email' => $email,
-            'password' => $hash,
-            'pseudo' => $pseudo,
-            'avatar' => $avatar
-        ];
-        $query->execute($parameters);
-        $user = $query->fetch(PDO::FETCH_ASSOC);
-
-        header('Location: ../index.php?route=home');
+        //if admin role has been given with register, save user with role admin
+        if ($role === 'admin') {
+            $query = $db->prepare('INSERT INTO users(pseudo, email, password, avatar, role) VALUES(:pseudo, :email, :password, :avatar, :role)');
+            $parameters = [
+                'email' => $email,
+                'password' => $hash,
+                'pseudo' => $pseudo,
+                'avatar' => $avatar,
+                'role' => $role
+            ];
+            $query->execute($parameters);
+            $user = $query->fetch(PDO::FETCH_ASSOC);
+            header('Location: ../index.php?route=admin');
+        } else {
+            //if no admin, give no role
+            $query = $db->prepare('INSERT INTO users(pseudo, email, password, avatar) VALUES(:pseudo, :email, :password, :avatar)');
+            $parameters = [
+                'email' => $email,
+                'password' => $hash,
+                'pseudo' => $pseudo,
+                'avatar' => $avatar
+            ];
+            $query->execute($parameters);
+            $user = $query->fetch(PDO::FETCH_ASSOC);
+            header('Location: ../index.php?route=home');
+        }
     };
 };
